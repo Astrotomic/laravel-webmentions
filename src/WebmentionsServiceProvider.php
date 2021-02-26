6ed47098cbc77aeebcd404cd454c4569651e14e7
Tom Witkowski <dev.gummibeer@gmail.com>
2021-02-26 20:53:46 +0100

    remove unused config

--- src/WebmentionsServiceProvider.php
+++ src/WebmentionsServiceProvider.php
@@ -6,19 +6,8 @@ use Illuminate\Support\ServiceProvider;
 
 class WebmentionsServiceProvider extends ServiceProvider
 {
-    public function boot(): void
-    {
-        if ($this->app->runningInConsole()) {
-            $this->publishes([
-                __DIR__.'/../config/config.php' => config_path('webmentions.php'),
-            ], 'config');
-        }
-    }
-
     public function register(): void
     {
-        $this->mergeConfigFrom(__DIR__.'/../config/config.php', 'webmentions');
-
         $this->app->singleton(Client::class);
     }
 }
